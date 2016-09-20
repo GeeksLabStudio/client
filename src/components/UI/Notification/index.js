b6e4a9d435b6416e8da444f7b26077ce2fa38530
apoddubny <apoddubny@speroteck.com>
2016-09-20 12:02:00 +0300

    UI/notifications

--- src/components/UI/Notification/index.js
+++ src/components/UI/Notification/index.js
@@ -3,17 +3,56 @@ require('./style.less');
 
 export default class Notification extends React.Component {
   static propTypes = {
-
+    message: React.PropTypes.string
   }
 
   state = {
+    messages: []
+  }
 
+  componentWillReceiveProps(newProps) {
+    this._push(newProps.message);
+  }
+
+  _remove() {
+    setTimeout(() => {
+      let messages = this.state.messages;
+      messages.splice(0, 1);
+
+      this.setState({
+        messages
+      })
+    }, 2500);
+  }
+
+  _push(message) {
+    let messages = this.state.messages;
+
+    messages.push(message);
+
+    this.setState({
+      messages
+    })
+
+    this._remove();
+  }
+
+  get _messages() {
+    let messages = this.state.messages;
+
+    return messages.map(function(el, i) {
+      return (
+        <div key={i} className="message">
+          {el}
+        </div>
+      );
+    })
   }
 
   render() {
     return (
-      <div className="notification">
-        
+      <div className="notifications">
+        {this._messages}
       </div>
     )
   }
