8e3d733e536efff88b25110d7b6fddba5cc39639
n4bcak3 <nabcak3@gmail.com>
2016-09-26 18:08:12 +0300

    updated notification story

--- src/components/UI/Notification/story.js
+++ src/components/UI/Notification/story.js
@@ -3,6 +3,19 @@ import { storiesOf, action } from '@kadira/storybook';
 import Notification from './index.js';
 
 storiesOf('UI', module)
-  .add('Notification', () => (
-    <Notification />
-  ))
+  .add('Notification', () => {
+    var _ref;
+    var i = 0;
+
+    var elem = <Notification
+      ref={ref => _ref = ref}
+    />
+
+    setInterval(()=>{
+      if (_ref){
+        _ref._push('test message #'+i++)
+      }
+    },500)
+
+    return elem
+  })
