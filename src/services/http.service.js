ac7551945af566b43e7144f2b6fa7534a47dbab4
n4bcak3 <nabcak3@gmail.com>
2016-10-13 19:10:01 +0300

    http.service init

---
+++ src/services/http.service.js
@@ -0,0 +1,17 @@
+// Some of the ideas about how it should be looked
+// $http.get({
+//   method: $http.get,
+//   url: config.api.login,
+//   data
+// }).then(data => {
+//
+// })
+
+class HttpService {
+
+
+}
+
+const $http = new HttpService();
+
+export default $http
